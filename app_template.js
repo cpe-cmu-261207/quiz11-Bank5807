ac885fba6ae90c772290014a8ed33aa6b65d0e60
Bank5807 <tanat_wip@cmu.ac.th>
2021-01-12 13:57:21 +0700

    modified:   app_template.js

--- app_template.js
+++ app_template.js
@@ -1,1 +1,12 @@
-//your code here!
+let id = getElementbyid(input-number);
+let botton = getElementbyclassname(button-compute);
+ 
+if(botton == true) {
+    console.log(subtract(id));
+}
+
+function subtract(number) {
+    let id = 620610787;
+    return id-number
+}
+
